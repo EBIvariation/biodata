c5feb3f9fa5637a2a6264d05a03d883a17b71435
Sundar Venkataraman <sundarvenkata@ebi.ac.uk>
2021-01-30 12:09:20 +0000

    Fix imports and use OpenJDK8 for Travis CI

--- biodata-models/src/test/java/org/opencb/biodata/models/variant/stats/VariantStatsTest.java
+++ biodata-models/src/test/java/org/opencb/biodata/models/variant/stats/VariantStatsTest.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.opencb.biodata.models.variant.Variant;
